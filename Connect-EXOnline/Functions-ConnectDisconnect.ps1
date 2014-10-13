25756ab820bfec93ca3f20b8f6a8474cc6d7e01b
Paul Cunningham <paul@locklan.com.au>
2014-10-14 08:57:52 +1000

    Functions-ConnectDisconnect.ps1 added
    Functions for Connecting/Disconnecting to Exchange Online

---
+++ Connect-EXOnline/Functions-ConnectDisconnect.ps1
@@ -0,0 +1,17 @@
+﻿Function Connect-EXOnline {
+
+    $URL = "https://ps.outlook.com/powershell"
+    
+    $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
+
+    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Office 365"
+
+    Import-PSSession $EXOSession
+
+}
+
+Function Disconnect-EXOnline {
+    
+    Remove-PSSession -Name "Exchange Online"
+
+}
