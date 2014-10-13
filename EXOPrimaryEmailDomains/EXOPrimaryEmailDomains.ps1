1cd200627cf89f1ad599360cdda5bdafc1b25ec5
Paul Cunningham <paul@locklan.com.au>
2014-10-14 08:50:15 +1000

    EXOPrimaryEmailDomains.ps1 script added

---
+++ EXOPrimaryEmailDomains/EXOPrimaryEmailDomains.ps1
@@ -0,0 +1,13 @@
+﻿$mailboxes = Get-Mailbox -ResultSize Unlimited | select PrimarySmtpAddress
+
+$domains = @()
+
+foreach ($mailbox in $mailboxes)
+{
+    $domain = $mailbox.PrimarySmtpAddress.Split("@")[1]
+
+    $domains += $domain
+}
+
+$domains | Group-Object | Select name,count
+
