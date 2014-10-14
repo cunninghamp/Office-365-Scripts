422049fcded6a235ecb4f65fbad8c31866b76145
Paul Cunningham <paul@locklan.com.au>
2014-10-14 16:05:40 +1000

    Some minor code cleanups

--- Connect-EXOnline/Functions-ConnectDisconnect.ps1
+++ Connect-EXOnline/Functions-ConnectDisconnect.ps1
@@ -1,15 +1,47 @@
-﻿Function Connect-EXOnline {
+﻿<#
+These functions can be added to your PowerShell profile to allow you to
+quickly and easily connect to Exchange Online (Office 365).
+
+For more info on how they are used see:
+http://exchangeserverpro.com/powershell-function-connect-office-365/
+http://exchangeserverpro.com/create-powershell-profile
+
+
+Written by: Paul Cunningham
+
+Find me on:
+
+* My Blog:	http://paulcunningham.me
+* Twitter:	https://twitter.com/paulcunningham
+* LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+* Github:	https://github.com/cunninghamp
+
+For more Exchange Server tips, tricks and news
+check out Exchange Server Pro.
+
+* Website:	http://exchangeserverpro.com
+* Twitter:	http://twitter.com/exchservpro
+
+Change Log:
+V1.00, 14/10/2014 - Initial version
+#>
+http://exchangeserverpro.com/powershell-function-connect-office-365/
+
+# This function will prompt for authentication and connect to Exchange Online 
+Function Connect-EXOnline {
 
     $URL = "https://ps.outlook.com/powershell"
     
     $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
 
-    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Office 365"
+    $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Exchange Online"
 
     Import-PSSession $EXOSession
 
 }
 
+#This function will disconnect the PS Session "Exchange Online". This may
+#leave other PS Sessions open to EXO if others have been created.
 Function Disconnect-EXOnline {
     
     Remove-PSSession -Name "Exchange Online"
