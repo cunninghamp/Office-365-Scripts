532c9effcbe23bfde2b505517cb2badb8ab4dd88
Paul Cunningham <paul@locklan.com.au>
2014-10-23 14:27:44 +1000

    CompareToOnPremises.ps1 added

---
+++ CompareToOnPremises/CompareToOnPremises.ps1
@@ -0,0 +1,16 @@
+﻿$cloudmailboxes = Import-CSV .\migration.csv
+
+foreach ($cloudmailbox in $cloudmailboxes)
+{
+    try {
+        Get-Mailbox $cloudmailbox -ErrorAction STOP
+        $mailboxstatus = "Found"
+
+    }
+    catch
+    {
+        $mailboxstatus = "Not found"
+    }
+
+    Write-Host "$cloudmailbox was $mailboxstatus"
+}
