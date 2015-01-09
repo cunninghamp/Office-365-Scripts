d2184f5b2fb9e460f63f7ce49b8433e5cc8883b4
Paul Cunningham <paul@locklan.com.au>
2015-01-09 11:07:40 +1000

    Get-ImmutableID.ps1 added

---
+++ Get-ImmutableID/Get-ImmutableID.ps1
@@ -0,0 +1,54 @@
+﻿#Get-ImmutableID.ps1
+#Written By: Paul Cunningham
+#
+#Change Log
+#V1.0, 9/1/2015 - Initial version
+#
+#.SYNOPSIS 
+#Calculates the immutable ID of an on-premises Active Directory user object.
+#
+#.EXAMPLE
+#.\Get-ImmutableID.ps1 user1@domain.com
+#
+
+#...................................
+# Static Variables
+#...................................
+
+
+
+#...................................
+# Functions
+#...................................
+
+
+
+#...................................
+# Script
+#...................................
+
+[CmdletBinding()]
+Param(
+    [Parameter(Mandatory=$True,Position=1)]
+    [string]$UserPrincipalName
+)
+
+try
+{
+    Import-Module ActiveDirectory -ErrorAction STOP
+    $user = Get-ADUser $UserPrincipalName -ErrorAction STOP
+    $guid = [GUID]($user).ObjectGUID
+    $bytearray = $guid.ToByteArray()
+
+    $immutableID = [system.convert]::ToBase64String($bytearray)
+
+    Write-Host "User: $userPrincipalName"
+    Write-Host "ImmutableID: $immutableID"
+
+}
+catch
+{
+    Write-Warning $_.Exception.Message
+    EXIT
+}
+
