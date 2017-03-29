d6e9d93ceb5f8be247789cdd2845377f28da3939
Paul Cunningham <paul@locklan.com.au>
2017-03-29 21:15:51 +1000

    Updated Connect-EXOnline to use stored creds

--- Connect-EXOnline/Functions-ConnectDisconnect.ps1
+++ Connect-EXOnline/Functions-ConnectDisconnect.ps1
@@ -25,19 +25,85 @@ check out Exchange Server Pro.
 Change Log:
 V1.00, 14/10/2014 - Initial version
 V1.01, 22/05/2016 - Updated with new PowerShell endpoint URL
+V1.02, 27/09/2016 - Connect-EXOnline updated with new functionality for using stored credentials
 #>
 
-# This function will prompt for authentication and connect to Exchange Online 
 Function Connect-EXOnline {
+
+    <#
+    .SYNOPSIS
+    Connect-EXOnline.ps1 - Connect to Exchange Online
+
+    .DESCRIPTION 
+    This function will connect to Exchange Online using PowerShell.
+
+    .PARAMETER UseCredential
+    Uses a stored credential on the computer for authenticating with
+    Exchange Online. For more information on using stored credentials
+    refer to
+
+    .PARAMETER Prefix
+    Prefixes the Exchange Online cmdlets with the value that you specify
+    (such as "Cloud")so that they can be used alongside the on-premises
+    Exchange cmdlets of the same name.
+
+    .EXAMPLE
+    Connect-EXOnline
+    Prompts for authentication and connects to Exchange Online.
+
+    .EXAMPLE
+    Connect-EXOnline -UseCredential admin@tenant.onmicrosoft.com
+    Uses the stored credential for admin@tenant.onmicrosoft.com to connect
+    to Exchange Online.
+
+    .EXAMPLE
+    Connect-EXOnline -UseCredential admin@tenant.onmicrosoft.com -Prefix EXO
+    Uses the stored credential for admin@tenant.onmicrosoft.com to connect
+    to Exchange Online, and prefixes the cmdlets with "EXO", for example
+    Get-EXOMailbox instead of Get-Mailbox.
+
+    .NOTES
+    Written by: Paul Cunningham
+
+    Find me on:
+
+    * My Blog:	http://paulcunningham.me
+    * Twitter:	https://twitter.com/paulcunningham
+    * LinkedIn:	http://au.linkedin.com/in/cunninghamp/
+    * Github:	https://github.com/cunninghamp
+
+    For more Office 365 tips, tricks and news
+    check out Practical 365.
+
+    * Website:	https://practical365.com
+    * Twitter:	https://twitter.com/practical365
+    #>
+
+    param(
+        [Parameter(Mandatory=$false)]
+        [string]$UseCredential,
+
+        [Parameter(Mandatory=$false)]
+        [string]$Prefix
+        )
 
     $URL = "https://outlook.office365.com/powershell-liveid/"
     
-    $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
+    if ($UseCredential) {
+        $Credentials = Get-StoredCredential -UserName $UseCredential
+    }
+    else {
+        $Credentials = Get-Credential -Message "Enter your Office 365 admin credentials"
+    }
 
     $EXOSession = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri $URL -Credential $Credentials -Authentication Basic -AllowRedirection -Name "Exchange Online"
 
-    Import-PSSession $EXOSession
-
+    if ($Prefix) {
+        Import-PSSession $EXOSession -Prefix $Prefix
+    }
+    else {
+        Import-PSSession $EXOSession
+    }
 }
 
 #This function will disconnect the PS Session "Exchange Online". This may
